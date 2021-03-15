e0ef6e91fa3e62db0b0b97824b67c33e57e2b3c2
Sabyasachi <43935997+sabrajsab@users.noreply.github.com>
2021-03-15 18:47:48 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+using namespace std;
+
+void solve()
+{
+  
+}
+
+int main(){
+    fast;
+    int t;
+    cin >> t;
+    while (t--) {
+      
+        solve();
+    }
+    return 0;
+}
